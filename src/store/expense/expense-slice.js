77dd2caa647bdc30afe99b851a22f34f8d5f1e93
Ike Odira <odiraike8@gmail.com>
2024-01-03 11:01:18 -0500

    Expense Tracker

---
+++ src/store/expense/expense-slice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const expenseSlice = createSlice({
+  name: "expenseSlice",
+  initialState: {
+    income: 100,
+    expenseList: [],
+  },
+  reducers: {
+    addExpenseAction: (currentSlice, action) => {
+      currentSlice.expenseList.push(action.payload);
+    },
+    setIncomeAction: (currentSlice, action) => {
+      currentSlice.income = action.payload;
+    },
+  },
+});
+
+export const { addExpenseAction, setIncomeAction } = expenseSlice.actions;
